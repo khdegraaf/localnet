bc3b00a3cfee24497ac586f79db7cd947b72bece
Wojciech <88704439+wojciech-sif@users.noreply.github.com>
2021-08-24 11:57:59 +0200

    Start simple tmux terminal (#4)

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/ridge/must"
+	"github.com/wojciech-malota-wojcik/ioc"
+	"github.com/wojciech-sif/localnet/exec"
+	"github.com/wojciech-sif/localnet/lib/run"
+)
+
+const sessionName = "localnet"
 
 func main() {
-	fmt.Println("test")
+	run.Tool("localnet", func(appRunner run.AppRunner, c *ioc.Container) {
+		appRunner(func(ctx context.Context) error {
+			if exec.TmateNoOut(ctx, "has-session", "-t", sessionName) != nil {
+				must.OK(exec.Tmate(ctx, "new-session", "-d", "-s", sessionName, "-n", "help", "bash"))
+			}
+			must.OK(exec.TmateTty(ctx, "attach-session", "-t", sessionName))
+			return nil
+		})
+	})
 }
--- exec/tmate.go
+++ exec/tmate.go
@@ -2,13 +2,16 @@ package exec
 
 import (
 	"context"
+	"io"
+	"os"
 	"os/exec"
 )
 
 var tmate string
 
 func init() {
-	for _, app := range []string{"tmate", "tmux"} {
+	// FIXME (wojciech): try to figure out why tmate can't attach to session with error "no current session"
+	for _, app := range []string{"tmux"} {
 		if _, err := exec.LookPath(app); err == nil {
 			tmate = app
 			break
@@ -19,4 +22,27 @@ func init() {
 // Tmate runs tmate command
 func Tmate(ctx context.Context, args ...string) error {
 	return Run(ctx, exec.Command(tmate, args...))
+}
+
+// TmateNoOut runs tmate command with discarded outputs
+func TmateNoOut(ctx context.Context, args ...string) error {
+	cmd := exec.Command(tmate, args...)
+	cmd.Stderr = io.Discard
+	cmd.Stdout = io.Discard
+	return Run(ctx, cmd)
+}
+
+// TmateTty runs tmate command with terminal attached
+func TmateTty(ctx context.Context, args ...string) error {
+	tty, err := os.OpenFile("/dev/pts/1", os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer tty.Close()
+
+	cmd := exec.Command(tmate, args...)
+	cmd.Stdin = tty
+	cmd.Stderr = tty
+	cmd.Stdout = tty
+	return Run(ctx, cmd)
 }

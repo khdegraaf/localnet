25dbad351a5ea6a177b9184c313ec467df095904
Wojciech <88704439+wojciech-sif@users.noreply.github.com>
2021-08-29 11:36:33 +0200

    List possible options for --target and --set automatically based on registered concretes in container (#34)

--- cmd/main.go
+++ cmd/main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/ridge/must"
 	"github.com/spf13/cobra"
+	"github.com/wojciech-malota-wojcik/ioc"
 	"github.com/wojciech-sif/localnet"
+	"github.com/wojciech-sif/localnet/infra"
 	"github.com/wojciech-sif/localnet/lib/run"
 )
 
 func main() {
-	run.Tool("localnet", localnet.IoC, func(configF *localnet.ConfigFactory, cmdF *localnet.CmdFactory) error {
+	run.Tool("localnet", localnet.IoC, func(c *ioc.Container, configF *localnet.ConfigFactory, cmdF *localnet.CmdFactory) error {
 		rootCmd := &cobra.Command{
 			Short: "Creates preconfigured bash session for environment",
 			RunE:  cmdF.Cmd(localnet.Activate),
 		}
 		rootCmd.PersistentFlags().StringVar(&configF.EnvName, "env", defaultString("LOCALNET_ENV", "localnet"), "Name of the environment to run in")
 		rootCmd.PersistentFlags().StringVar(&configF.HomeDir, "home", defaultString("LOCALNET_HOME", must.String(os.UserHomeDir())+"/.localnet"), "Directory where all files created automatically by localnet are stored")
-		rootCmd.PersistentFlags().StringVar(&configF.Target, "target", defaultString("LOCALNET_TARGET", "tmux"), "Target of the deployment (tmux | docker)")
+		rootCmd.PersistentFlags().StringVar(&configF.Target, "target", defaultString("LOCALNET_TARGET", "tmux"), "Target of the deployment: "+strings.Join(c.Names((*infra.Target)(nil)), " | "))
 		rootCmd.PersistentFlags().StringVar(&configF.BinDir, "bin-dir", defaultString("LOCALNET_BIN_DIR", must.String(os.UserHomeDir())+"/go/bin"), "Path to directory where executables exist")
 		rootCmd.PersistentFlags().StringVar(&configF.TMuxNetwork, "tmux-network", defaultString("LOCALNET_TMUX_NETWORK", "127.1.0.0"), "Network where IPs for applications are taken from")
-		rootCmd.Flags().StringVar(&configF.SetName, "set", defaultString("LOCALNET_SET", "dev"), "Application set to deploy (dev | single)")
+		rootCmd.Flags().StringVar(&configF.SetName, "set", defaultString("LOCALNET_SET", "dev"), "Application set to deploy: "+strings.Join(c.Names((*infra.Set)(nil)), " | "))
 
 		startCmd := &cobra.Command{
 			Use:   "start",
 			Short: "Starts dev environment",
 			RunE:  cmdF.Cmd(localnet.Start),
 		}
-		startCmd.Flags().StringVar(&configF.SetName, "set", defaultString("LOCALNET_SET", "dev"), "Application set to deploy (dev | single)")
+		startCmd.Flags().StringVar(&configF.SetName, "set", defaultString("LOCALNET_SET", "dev"), "Application set to deploy: "+strings.Join(c.Names((*infra.Set)(nil)), " | "))
 		rootCmd.AddCommand(startCmd)
 
 		rootCmd.AddCommand(&cobra.Command{
--- go.mod
+++ go.mod
@@ -15,6 +15,6 @@ require (
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
 	github.com/wojciech-malota-wojcik/build v0.0.0-20210131144749-3ef5b00b908f
-	github.com/wojciech-malota-wojcik/ioc v1.3.1-0.20210124163806-1a91e377508b
+	github.com/wojciech-malota-wojcik/ioc v1.3.1-0.20210829092813-3edb43f522c7
 	go.uber.org/zap v1.19.0
 )
